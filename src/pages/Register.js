340f574481a97c63b864b878dd28f958693d7f51
Samuel Tuka <samueltuka0@gmail.com>
2023-03-18 01:17:59 +0100

    Added registration formi validation

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,6 +4,8 @@ import { ContentNotLogged } from "../components/ContentNotLogged";
 import { ShowPasswordTextField } from "../components/ShowPasswordTextField";
 import { AuthenticationPageLink } from "../components/AuthenticationPageLink";
 import { EmailTextField } from "../components/EmailTextField";
+import { Formik, Form } from "formik";
+import * as yup from "yup";
 
 import {
   TextField,
@@ -44,119 +46,167 @@ const gridItemStyle = {
   alignItems: "center",
 };
 
+const validationSchema = yup.object({
+  firstName: yup
+    .string()
+    .matches(
+      /^[A-Z][a-zA-Z]{2,}$/,
+      "Krstné meno musí začínať veľkým písmenom, musí mať aspoň 3 znaky a môže obsahovať len písmená abecedy"
+    )
+    .required("Meno musí byť vyplnené"),
+  lastName: yup
+    .string()
+    .matches(
+      /^[A-Z][a-zA-Z]{2,}$/,
+      "Priezvisko musí začínať veľkým písmenom, musí mať aspoň 3 znaky a môže obsahovať len písmená abecedy"
+    )
+    .required("Meno musí byť vyplnené"),
+  email: yup
+    .string()
+    .required("Email musí byť vyplnený")
+    .matches(
+      /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+      "Neplatný tvar emailovej adresy"
+    ),
+  password: yup
+    .string()
+    .required("Heslo musí byť vyplnené")
+    .min(8, "Heslo musí obsahovať aspoň 8 znakov")
+    .matches(
+      /^(?=.*[A-Z])(?=.*\d).+$/,
+      "Heslo musí obsahovať aspoň jedno veľké písmeno a jedno číslo"
+    ),
+  repeatPassword: yup
+    .string()
+    .required("Heslo musíte potvrdiť")
+    .oneOf([yup.ref("password")], "Heslá sa nezhodujú"),
+});
+
 export const Register = () => {
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    repeatPassword: "",
-  });
-
-  const handleRegister = async (event) => {
-    event.preventDefault();
-    const response = await fetch("http://localhost:3000/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-
-    console.log(await response.json());
-  };
-
   return (
     <>
       <ContentNotLogged>
         <Container maxWidth="md" sx={containerStyle}>
           <Paper>
-            <Grid container sx={gridContainerStyle}>
-              <Grid item xs={12} sx={gridItemStyle}>
-                <Typography
-                  variant="h2"
-                  textTransform="uppercase"
-                  sx={{
-                    fontSize: "1.5rem",
-                    letterSpacing: "0.2rem",
-                    [appTheme.breakpoints.down("md")]: {
-                      fontSize: "1.1rem",
-                      letterSpacing: "0.1rem",
-                    },
-                  }}
-                >
-                  Registrácia používateľa
-                </Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  id="user-firstname"
-                  label="Zadajte meno"
-                  onChange={(event) =>
-                    setFormData({
-                      ...formData,
-                      firstName: event.target.value,
-                    })
-                  }
-                  sx={{ width: "100%" }}
-                ></TextField>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  id="user-lastname"
-                  label="Zadajte priezvisko"
-                  onChange={(event) =>
-                    setFormData({
-                      ...formData,
-                      lastName: event.target.value,
-                    })
-                  }
-                  sx={{ width: "100%" }}
-                ></TextField>
-              </Grid>
-              <Grid item xs={12}>
-                <EmailTextField
-                  id="email"
-                  label="Zadajte email"
-                  setFormData={setFormData}
-                  formData={formData}
-                ></EmailTextField>
-              </Grid>
-              <Grid item xs={12}>
-                <ShowPasswordTextField
-                  id="password"
-                  label="Zadajte heslo"
-                  setFormData={setFormData}
-                  formData={formData}
-                ></ShowPasswordTextField>
-              </Grid>
-              <Grid item xs={12}>
-                <ShowPasswordTextField
-                  id="repeatPassword"
-                  label="Zadajte heslo znovu"
-                  setFormData={setFormData}
-                  formData={formData}
-                ></ShowPasswordTextField>
-              </Grid>
-              <Grid item xs={12} md={12} sx={gridItemStyle}>
-                <AuthenticationPageLink
-                  text="Už máte účet?"
-                  to="/login"
-                  boldText="Prihláste sa"
-                ></AuthenticationPageLink>
-              </Grid>
-              <Grid item xs={12} sx={gridItemStyle}>
-                <Button
-                  sx={{ padding: "1rem" }}
-                  variant="contained"
-                  onClick={handleRegister}
-                >
-                  Registrovať sa
-                </Button>
-              </Grid>
-            </Grid>
+            <Formik
+              initialValues={{
+                firstName: "",
+                lastName: "",
+                email: "",
+                password: "",
+                repeatPassword: "",
+              }}
+              onSubmit={(values) => console.log(values)}
+              validationSchema={validationSchema}
+            >
+              {({ values, handleChange, errors, touched, handleBlur }) => (
+                <Form>
+                  <Grid container sx={gridContainerStyle}>
+                    <Grid item xs={12} sx={gridItemStyle}>
+                      <Typography
+                        variant="h2"
+                        textTransform="uppercase"
+                        sx={{
+                          fontSize: "1.5rem",
+                          letterSpacing: "0.2rem",
+                          [appTheme.breakpoints.down("md")]: {
+                            fontSize: "1.1rem",
+                            letterSpacing: "0.1rem",
+                          },
+                        }}
+                      >
+                        Registrácia používateľa
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        id="firstName"
+                        name="firstName"
+                        label="Zadajte meno"
+                        value={values.firstName}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        error={touched.firstName && Boolean(errors.firstName)}
+                        helperText={touched.firstName && errors.firstName}
+                        sx={{ width: "100%" }}
+                      ></TextField>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        id="lastName"
+                        name="lastName"
+                        value={values.lastName}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        error={touched.lastName && Boolean(errors.lastName)}
+                        helperText={touched.lastName && errors.lastName}
+                        label="Zadajte priezvisko"
+                        sx={{ width: "100%" }}
+                      ></TextField>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <EmailTextField
+                        id="email"
+                        label="Zadajte email"
+                        name="email"
+                        value={values.email}
+                        handleChange={handleChange}
+                        handleBlur={handleBlur}
+                        error={touched.email && Boolean(errors.email)}
+                        helperText={touched.email && errors.email}
+                      ></EmailTextField>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <ShowPasswordTextField
+                        id="password"
+                        label="Zadajte heslo"
+                        name="password"
+                        value={values.password}
+                        handleChange={handleChange}
+                        handleBlur={handleBlur}
+                        error={touched.password && Boolean(errors.password)}
+                        helperText={touched.password && errors.password}
+                      ></ShowPasswordTextField>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <ShowPasswordTextField
+                        id="repeatPassword"
+                        label="Zadajte heslo"
+                        name="repeatPassword"
+                        value={values.repeatPassword}
+                        handleChange={handleChange}
+                        handleBlur={handleBlur}
+                        error={
+                          touched.repeatPassword &&
+                          Boolean(errors.repeatPassword)
+                        }
+                        helperText={
+                          touched.repeatPassword && errors.repeatPassword
+                        }
+                      ></ShowPasswordTextField>
+                    </Grid>
+                    <Grid item xs={12} md={12} sx={gridItemStyle}>
+                      <AuthenticationPageLink
+                        text="Už máte účet?"
+                        to="/login"
+                        boldText="Prihláste sa"
+                      ></AuthenticationPageLink>
+                    </Grid>
+                    <Grid item xs={12} sx={gridItemStyle}>
+                      <Button
+                        sx={{ padding: "1rem" }}
+                        variant="contained"
+                        type="submit"
+                      >
+                        Registrovať sa
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Form>
+              )}
+            </Formik>
           </Paper>
         </Container>
       </ContentNotLogged>
