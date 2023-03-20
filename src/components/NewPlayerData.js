c58c0c6c6ff6377bde1f73c11ef0bb9afba2798f
Samuel Tuka <samueltuka0@gmail.com>
2023-03-20 03:03:53 +0100

    Changed so it accepts only csv files

--- src/components/NewPlayerData.js
+++ src/components/NewPlayerData.js
@@ -212,7 +212,52 @@ export const NewPlayerData = (props) => {
             alignItems: "center",
           }}
         >
-          <Input type="file" onChange={(e) => handleFileChange(e)} />
+          <Grid container>
+            <Grid
+              item
+              xs={12}
+              textAlign="center"
+              sx={{ marginBottom: "0.4rem" }}
+            >
+              <Typography sx={{ marginBottom: "1rem" }}>
+                Zoznam vaších skupín
+              </Typography>
+            </Grid>
+            <Grid
+              container
+              item
+              xs={12}
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                marginBottom: "1rem",
+              }}
+              spacing={2}
+            >
+              {groups.map((group) => (
+                <Grid key={group.group_name} item xs={6} sm={4}>
+                  <Paper sx={{ padding: "0.3rem" }}>
+                    <Typography sx={{ textAlign: "center" }}>
+                      {group.group_name}
+                    </Typography>
+                  </Paper>
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+          <Typography textAlign="center" sx={{ marginBottom: "0.5rem" }}>
+            Každý riadok v súbore .csv musí byť v tvare:
+          </Typography>
+          <Typography textAlign="center" sx={{ marginBottom: "0.5rem" }}>
+            Meno, Priezvisko, email, SKUPINA - ?, elo kde každá hodnota je jedna
+            bunka
+          </Typography>
+          <Input
+            type="file"
+            inputProps={{ accept: "text/csv" }}
+            onChange={(e) => handleFileChange(e)}
+          />
         </Box>
       )}
     </>
