c6e5c8a7cea4f8b8f569dafbba3c0c9a2a87179d
Samuel Tuka <samueltuka0@gmail.com>
2023-04-01 13:26:05 +0200

    Added comments to axios file

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,5 +1,7 @@
+// Naimportovany balicek pre spracovanie requestov
 import axios from "axios";
 
+// Globalne nastavenie - URL serveru a nastavenie aby sa posielali cookies
 export const api = axios.create({
   baseURL: "http://localhost:3000",
   withCredentials: true,
